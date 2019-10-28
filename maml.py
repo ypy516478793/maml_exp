Fix the bug: gradient is None for the first layer

maml.py
@@ -121,9 +121,9 @@ class MAML:
 
                 return task_output
 
-            if FLAGS.norm is not 'None':
-                # to initialize the batch norm vars, might want to combine this, and not run idx 0 twice.
-                unused = task_metalearn((self.inputa[0], self.inputb[0], self.labela[0], self.labelb[0]), False)
+            # if FLAGS.norm is not 'None':
+            #     # to initialize the batch norm vars, might want to combine this, and not run idx 0 twice.
+            #     unused = task_metalearn((self.inputa[0], self.inputb[0], self.labela[0], self.labelb[0]), False)
 
             out_dtype = [tf.float32, [tf.float32]*num_updates, tf.float32, [tf.float32]*num_updates]
             if self.classification:
@@ -195,24 +195,26 @@ class MAML:
 
     def forward_fc_dropconnect(self, inp, weights, reuse=False, keep_prob=None):
         if keep_prob is not None:
-            shape_w1 = weights['w1'].shape
-            shape_b1 = weights['b1'].shape
+            # shape_w1 = weights['w1'].shape
+            # shape_b1 = weights['b1'].shape
             drop_weights_w1 = tf.nn.dropout(weights['w1'], keep_prob=keep_prob) * keep_prob
             drop_weights_b1 = tf.nn.dropout(weights['b1'], keep_prob=keep_prob) * keep_prob
-            weights_w1 = tf.reshape(drop_weights_w1, shape=shape_w1)
-            weights_b1 = tf.reshape(drop_weights_b1, shape=shape_b1)
-            hidden = normalize(tf.matmul(inp, weights_w1) + weights_b1, activation=tf.nn.relu, reuse=reuse, scope='0')
+            # weights_w1 = tf.reshape(drop_weights_w1, shape=shape_w1)
+            # weights_b1 = tf.reshape(drop_weights_b1, shape=shape_b1)
+            # hidden = normalize(tf.matmul(inp, weights_w1) + weights_b1, activation=tf.nn.relu, reuse=reuse, scope='0')
+            hidden = normalize(tf.matmul(inp, drop_weights_w1) + drop_weights_b1, activation=tf.nn.relu, reuse=reuse, scope='0')
         else:
             hidden = normalize(tf.matmul(inp, weights['w1']) + weights['b1'], activation=tf.nn.relu, reuse=reuse, scope='0')
         for i in range(1,len(self.dim_hidden)):
             if keep_prob is not None:
-                shape_w_i = weights['w'+str(i+1)].shape
-                shape_b_i = weights['b'+str(i+1)].shape
+                # shape_w_i = weights['w'+str(i+1)].shape
+                # shape_b_i = weights['b'+str(i+1)].shape
                 drop_weights_w_i = tf.nn.dropout(weights['w'+str(i+1)], keep_prob=keep_prob) * keep_prob
                 drop_weights_b_i = tf.nn.dropout(weights['b'+str(i+1)], keep_prob=keep_prob) * keep_prob
-                weights_w_i = tf.reshape(drop_weights_w_i, shape=shape_w_i)
-                weights_b_i = tf.reshape(drop_weights_b_i, shape=shape_b_i)
-                hidden = normalize(tf.matmul(inp, weights_w_i) + weights_b_i, activation=tf.nn.relu, reuse=reuse, scope=str(i+1))
+                # weights_w_i = tf.reshape(drop_weights_w_i, shape=shape_w_i)
+                # weights_b_i = tf.reshape(drop_weights_b_i, shape=shape_b_i)
+                # hidden = normalize(tf.matmul(hidden, weights_w_i) + weights_b_i, activation=tf.nn.relu, reuse=reuse, scope=str(i+1))
+                hidden = normalize(tf.matmul(hidden, drop_weights_w_i) + drop_weights_b_i, activation=tf.nn.relu, reuse=reuse, scope=str(i+1))
             else:
                 hidden = tf.nn.dropout(hidden,  keep_prob=keep_prob)
             hidden = normalize(tf.matmul(hidden, weights['w'+str(i+1)]) + weights['b'+str(i+1)], activation=tf.nn.relu, reuse=reuse, scope=str(i+1))